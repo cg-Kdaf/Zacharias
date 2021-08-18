56fbe672c001d4b43ab8193c8b1438625dba84ea
cg-Kdaf <kdblender@gmail.com>
2021-08-18 18:56:27 +0200

    Docstring/Codeclearing + test chunks Adding the ability to work by chunks for the intent test, to allow addings without breaking the oldest passed tests.

--- src/spacy_tools.py
+++ src/spacy_tools.py
@@ -1,64 +1,71 @@
-import spacy
-from spacy import displacy
+from spacy import load as spacy_load
+# from spacy import displacy
 from os import path
-# from nltk.corpus import wordnet as wn
 
-print("start\n\n")
 project_root = path.split(path.dirname(path.abspath(__file__)))[0]
 
-
-# def find_antonyms(word):
-#     antonyms = []
-#     for syn_set in wn.synsets("have"):
-#         for l in syn_set.lemmas():
-#             if l.antonyms():
-#                 antonyms.append(l.antonyms()[0].name())
-#     return list(dict.fromkeys(antonyms))
+NLP_MODEL = spacy_load("en_core_web_md")
 
 
-nlp = spacy.load("en_core_web_lg")
+def intent_to_action(speech: str, actions: list):
+    """This function tries to find an action from the speech. It uses Spacy similarity tool.
 
-
-def find_corresponding_action(speech, actions, minimum):
-    highest_rate = 0
-    speech_nlp = nlp(speech)
-    highest_rate_id = 0
-    power = 1. if any(doc.pos_ == "NOUN" for doc in speech_nlp) else 0.8
+    Args:
+        speech (string): Sentence to be compared to the actions
+        actions (string list): List of sentences that describe each actions
+    Returns:
+        action_id (int): the most probable action that matches the speech
+        action_probability (float): probability that the speech matches this action
+        probability_multiplier (float): multiplier to lower the probability if sentence data do not
+        help to find a corresponding action
+    """
+    action_probability = 0.
+    speech_nlp = NLP_MODEL(speech)
+    action_id = 0
+    probability_multiplier = 1. if any(doc.pos_ == "NOUN" for doc in speech_nlp) else 0.8
     for id, action in enumerate(actions):
-        factor = nlp(action).similarity(speech_nlp)
-        if factor > minimum and factor > highest_rate:
-            highest_rate_id = id
-            highest_rate = factor
+        factor = float(NLP_MODEL(action).similarity(speech_nlp))
+        if factor > action_probability:
+            action_id = id
+            action_probability = factor
     # displacy.serve(speech_nlp, style="dep")
-    return(highest_rate_id, highest_rate, power)
+    return(action_id, action_probability, probability_multiplier)
 
 
-def split_sentence_clauses(text):
-    # TODO add proper descriptions everywhere and clean the code. Also remove unused lines.
-    seen = set() # keep track of covered words
-    doc = nlp(text)
-    chunks = []
-    for sent in doc.sents:
-        # for cc in sent.root.children:
-        #     print(cc.dep_)
-        heads = [cc for cc in sent.root.children if cc.dep_ in ['conj', "ccomp"]]
+def split_sentence_clauses(sentences):
+    """Split the given sentence(s) into a list of clauses. This is using the dependencies given by
+    spacy, but the accurary is not optimal since clauses detection is very hard toachieve right.
+
+    This functions helps for example to try sentence similarities with different portions of the
+    sentence in order to have more accurate results.
+
+    TODO : Better restituate the punctuation.
+
+    Args:
+        sentence (str): Sentence(s) to split
+    Returns:
+        clauses_list (str list)
+    """
+    seen = set()  # keep track of covered words
+    doc = NLP_MODEL(sentences)
+    clauses = []
+    for sentence in doc.sents:  # Split into sentences if the input string is a text
+        heads = [cc for cc in sentence.root.children if cc.dep_ in ['conj', "ccomp"]]
 
         for head in heads:
             words = [ww for ww in head.subtree]
             for word in words:
                 seen.add(word)
-            chunk = (' '.join([ww.text for ww in words]))
-            chunks.append( (head.i, chunk) )
+            clause = (' '.join([ww.text for ww in words]))
+            clauses.append((head.i, clause))
 
-        unseen = [ww for ww in sent if ww not in seen]
-        chunk = ' '.join([ww.text for ww in unseen])
-        chunks.append( (sent.root.i, chunk) )
+        unseen = [ww for ww in sentence if ww not in seen]
+        clause = ' '.join([ww.text for ww in unseen])
+        clauses.append((sentence.root.i, clause))
 
-    chunks = sorted(chunks, key=lambda x: x[0])
+    clauses = sorted(clauses, key=lambda x: x[0])
 
-    # print([chunk[1] for chunk in chunks])
-    return([chunk[1] for chunk in chunks])
-    # displacy.serve(doc, style="dep")
+    return([clause[1] for clause in clauses])
 
 
 def test_samples():
@@ -69,31 +76,38 @@ def test_samples():
     sentences_ = sentences_.readlines()
     sentences = []
     for id in range(len(sentences_)):
-        if sentences_[id] != "\n":
+        if sentences_[id] == "-- Checkpoint":
+            sentences.append("Checkpoint")
+        elif sentences_[id] != "\n":
             action_id, sentence = sentences_[id].replace("\n", "").split(" - ")
             action_id = int(action_id)
             sentences.append([action_id, sentence])
-    # example = sentences[14][1]
-    # split_sentence_clauses(example)
-    # exit()
 
-    error_length = 0
+    error_len = 0
+    chunk = 0
+    chunk_len = 0
     for sentence in sentences:
+        if sentence == "Checkpoint":
+            print(f"Error rate for chunk {chunk}: {error_len/chunk_len}")
+            chunk += 1
+            chunk_len = 0
+            continue
         clauses = split_sentence_clauses(sentence[1])
         found_action_id = 0
         factor = 0.
         for clause in clauses:
-            id, fac, power = find_corresponding_action(clause, actions, 0.7)
+            id, fac, power = intent_to_action(clause, actions)
             if fac*power > factor:
                 # print(clause)
                 found_action_id, factor = id, fac*power
+        chunk_len += 1
         if found_action_id != sentence[0]-1:
             print(clauses, factor)
             print(f"'{sentence[1]}'   ->   Error")
-            print(f"found : \n   {actions[found_action_id]} \ninstead of\n   {actions[sentence[0]-1]} \n")
-            error_length += 1
-            # displacy.serve(nlp(sentence[1]), style="dep")
-    print(f"Error rate : {error_length/len(sentences)}")
+            print(f"""found : \n   {actions[found_action_id]}
+                      \rinstead of\n   {actions[sentence[0]-1]} \n""")
+            error_len += 1
+            # displacy.serve(NLP_MODEL(sentence[1]), style="dep")  # Used to analyse the error
 
 
 test_samples()
